13564573fc0075fb8d7cdf212900d3795e194acd
Cvolton <cvolton@cvolton.eu>
2023-08-23 00:09:17 +0200

    show play replay button after downloading replay

--- src/hooks/LevelInfoLayer.cpp
+++ src/hooks/LevelInfoLayer.cpp
@@ -16,12 +16,8 @@ class $modify(RBLevelInfoLayer, LevelInfoLayer) {
         LevelInfoLayer::onPlay(sender);
     }
 
-    /*
-     * Hooks
-     */
-
-    bool init(GJGameLevel* level) {
-        if(!LevelInfoLayer::init(level)) return false;
+    void createReplayButton() {
+        if(this->getChildByID("rb-play-menu")) return;
 
         if(!std::string(m_level->m_recordString).empty()) {
             auto winSize = CCDirector::sharedDirector()->getWinSize();
@@ -42,7 +38,28 @@ class $modify(RBLevelInfoLayer, LevelInfoLayer) {
         } else {
             log::info("no replay available for this level");
         }
+    }
+
+    /*
+     * Hooks
+     */
+
+    bool init(GJGameLevel* level) {
+        if(!LevelInfoLayer::init(level)) return false;
+
+        this->createReplayButton();
 
         return true;
     }
+
+
+    void levelDownloadFinished(GJGameLevel* level) {
+        LevelInfoLayer::levelDownloadFinished(level);
+        createReplayButton();
+    };
+
+    void levelUpdateFinished(GJGameLevel* level, UpdateResponse response) {
+        LevelInfoLayer::levelUpdateFinished(level, response);
+        createReplayButton();
+    };
 };
